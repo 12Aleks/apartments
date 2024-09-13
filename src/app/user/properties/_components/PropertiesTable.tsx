9be728021ba878226701f16482071d4ea4939e59
12Aleks <tanais2010@yahoo.com>
2024-09-13 19:14:58 +0200

    A list of properties has been added

---
+++ src/app/user/properties/_components/PropertiesTable.tsx
@@ -0,0 +1,69 @@
+"use client"
+import {Prisma} from "@prisma/client";
+
+import {Table, TableBody, TableColumn, TableHeader, TableRow, TableCell, Tooltip, Pagination} from "@nextui-org/react";
+import Link from "next/link";
+import {PencilIcon, TrashIcon, EyeIcon} from "@heroicons/react/16/solid";
+import {useRouter} from "next/navigation";
+
+type Props = {
+    //Tak kak property tip i dannyje otliczajutsa modificyrujem tip dannych
+    properties: Prisma.PropertyGetPayload<{
+        include: {
+            type: true;
+            status: true;
+        }
+    }>[],
+    totalPages: number,
+    currentPage: number
+}
+
+const PropertiesTable = ({properties, totalPages, currentPage}: Props) => {
+    const router = useRouter();
+
+    return (
+        <>
+        <Table className="rounded-t-none">
+            <TableHeader>
+                <TableColumn>Name</TableColumn>
+                <TableColumn>Price</TableColumn>
+                <TableColumn>Type</TableColumn>
+                <TableColumn>Status</TableColumn>
+                <TableColumn>Actions</TableColumn>
+            </TableHeader>
+            <TableBody>
+                {
+                    properties.map(item => <TableRow key={item.id}>
+                        <TableCell>{item.name}</TableCell>
+                        <TableCell>{item.price}</TableCell>
+                        <TableCell>{item.type.value}</TableCell>
+                        <TableCell>{item.status.value}</TableCell>
+                        <TableCell>
+                            <div className="flex items-center gap-4">
+                                <Tooltip content="Detals">
+                                <Link href={`/property/${item.id}`}><EyeIcon className="w-5 text-state-500"></EyeIcon></Link>
+                                </Tooltip>
+                                <Tooltip content="Edit property" color="warning">
+                                    <Link href={`/user/property/${item.id}/edit`}>
+                                        <PencilIcon className="w-5 text-yellow-500"></PencilIcon>
+                                    </Link>
+                                </Tooltip>
+                                <Tooltip content="Delete property" color="danger">
+                                    <Link href={`/user/property/${item.id}/delete`}>
+                                        <TrashIcon className="w-5 text-red-500"></TrashIcon>
+                                    </Link>
+                                </Tooltip>
+                            </div>
+                        </TableCell>
+                    </TableRow>)
+                }
+            </TableBody>
+        </Table>
+            <Pagination total={totalPages} initialPage={1} page={currentPage} className="mt-3 flex flex-col items-center gap-4"
+            onChange={(page: number) => router.push(`/user/properties?pagenum=${page}`)}
+            />
+        </>
+    );
+};
+
+export default PropertiesTable;
--- src/app/user/properties/page.tsx
+++ src/app/user/properties/page.tsx
@@ -1,9 +1,49 @@
-const UnitsPage = () => {
-    return (
-        <div>
-            
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import prisma from "@/lib/prisma";
+import PropertiesTable from "@/app/user/properties/_components/PropertiesTable";
+import BlockTitle from "@/app/components/blockTitle";
+
+const PAGE_SIZE = 12;
+
+
+
+interface Props{
+    searchParams: {
+        [key: string]: string | string[] | undefined;
+    }
+}
+
+const PropertiesPage = async({searchParams}: Props) => {
+    const {getUser} = await getKindeServerSession();
+    const user = await getUser();
+    const pagenum = searchParams.pagenum ?? 0;
+
+    const propertiesPromise =  await prisma.property.findMany({
+        where: {
+            userId: user.id,
+        },
+        include: {
+            type: true,
+            status: true,
+
+        },
+        skip: +pagenum * PAGE_SIZE,
+        take: PAGE_SIZE
+    });
+  const totalPropertiesPromise =  prisma.property.count({
+      where: {
+          userId: user.id,
+      }
+  })
+    const [properties, totalProperties] = await Promise.all([propertiesPromise, totalPropertiesPromise]);
+
+    const totalPages = Math.floor(totalProperties / PAGE_SIZE);
+
+
+    return <div className="p-3 h-100">
+        <BlockTitle title={"Properties list"} className="mb-3"/>
+        <PropertiesTable properties={properties} totalPages={totalPages} currentPage={+pagenum} />
         </div>
-    );
 };
 
-export default UnitsPage;
+export default PropertiesPage;
